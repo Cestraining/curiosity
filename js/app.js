86971d2970f44f0c0b4ed0c2651e50d039691537
kalikali <localhai4@gmail.com>
2020-10-21 16:50:25 +0530

    first

---
+++ js/app.js
@@ -0,0 +1,49 @@
+//modal
+const modalwa =document.getElementById('modal');
+
+//open modal
+const getstart= document.getElementById('getstarted');
+const signin= document.getElementById('signin');
+
+//modal two buttons
+const btnregis=document.getElementById('btnregis');
+const btnlog=document.getElementById('btnlog');
+
+let rc=btnregis.style;
+let lc=btnlog.style;
+
+//modal two forms
+const form2=document.getElementById('form2');
+const form1=document.getElementById('form1');
+
+// login
+let loga=["none","block","red",""];
+signin.addEventListener("click" , function(){ openwa(loga); });
+btnlog.addEventListener('click',function(){ openwa(loga); });
+
+//signup
+let regisa=["block","none","","red"];
+getstart.addEventListener('click', function(){ openwa(regisa); });
+btnregis.addEventListener('click',function(){ openwa(regisa); });
+
+
+//close modal style1
+window.onclick=function(event){
+    if(event.target== modalwa){
+        lc.backgroundColor="";
+        rc.backgroundColor="";
+        modalwa.style.display="none";
+        
+    }
+};
+
+//open modal with values
+function openwa(pa)
+{
+    modalwa.style.display="block";
+    form1.style.display=pa[0];
+     form2.style.display=pa[1];
+
+     lc.backgroundColor=pa[2];
+     rc.backgroundColor=pa[3];
+}
