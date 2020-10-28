829c7aa8d73244311a3785af20d8a5f913baff93
kalikali <localhai4@gmail.com>
2020-10-28 12:07:56 +0530

    added icons for different section in both admin and student

--- admin/adminaside.php
+++ admin/adminaside.php
@@ -1,14 +1,14 @@
 <aside class="noprint">
 <h2 style="margin:5px;color:blue;">Admin Panel</h2><hr><br>
 
-      <a id="Dashboard" href="admin_dashboard.php">Dashboard</a>
-      <a id="Courses" href="admin_courses.php">Courses</a>
-      <a id="Lessons" href="admin_lessons.php">Lessons</a>
-      <a id="Students" href="admin_stu.php">Students</a>
-      <a id="Enrolls" href="enroll_report.php">Enroll Report</a>
-      <a id="Feedbacks" href="admin_feed.php">FeedBack</a>
-      <a id="Change Password" href="admin_chPass.php">Change Password</a>
-      <a href="ad_logout.php">Logout</a>
+      <a id="Dashboard" href="admin_dashboard.php"><img class="icons" src="../images/clipboard.svg"></img> Dashboard</a>
+      <a id="Courses" href="admin_courses.php"><img class="icons" src="../images/book.svg"></img> Courses</a>
+      <a id="Lessons" href="admin_lessons.php"><img class="icons" src="../images/book-open.svg"></img> Lessons</a>
+      <a id="Students" href="admin_stu.php"><img class="icons" src="../images/users.svg"></img> Students</a>
+      <a id="Enrolls" href="enroll_report.php"><img class="icons" src="../images/money-check.svg"></img> Enroll Report</a>
+      <a id="Feedbacks" href="admin_feed.php"><img class="icons" src="../images/file-invoice.svg"></img> FeedBack</a>
+      <a id="Change Password" href="admin_chPass.php"><img class="icons" src="../images/key.svg"></img> Change Password</a>
+      <a href="ad_logout.php"><img class="icons" src="../images/sign-out-alt.svg"></img> Logout</a>
     </aside>
     <script>
       document.getElementById(document.title).style.backgroundColor="blue";
--- student/stuaside.php
+++ student/stuaside.php
@@ -4,12 +4,12 @@
         $result=$conn->query("SELECT stu_img FROM student WHERE stu_id=$id");
         $row=$result->fetch_assoc();
       ?>
-      <img src="<?php echo $row['stu_img'] ?>" alt="">
-      <a id="Profile" href="studentProfile.php">Profile</a>
-      <a id="Courses" href="myCourses.php">My Courses</a>
-      <a id="FeedBack" href="stufeedBack.php">FeedBack</a>
-      <a id="Change Password" href="stuChangePass.php">Change Password</a>
-      <a href="stu_logout.php">Logout</a>
+      <img class="profile_img" src="<?php echo $row['stu_img'] ?>" alt="">
+      <a id="Profile" href="studentProfile.php"><img class="icons" src="../images/user-alt.svg"></img> Profile</a>
+      <a id="Courses" href="myCourses.php"><img class="icons" src="../images/book.svg"></img> My Courses</a>
+      <a id="FeedBack" href="stufeedBack.php"><img class="icons" src="../images/file-invoice.svg"></img> FeedBack</a>
+      <a id="Change Password" href="stuChangePass.php"><img class="icons" src="../images/key.svg"></img> Change Password</a>
+      <a href="stu_logout.php"><img class="icons" src="../images/sign-out-alt.svg"></img> Logout</a>
     </aside>
     <script>
       document.getElementById(document.title).style.backgroundColor="blue";
